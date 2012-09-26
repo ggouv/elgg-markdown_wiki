1f830e10e469ac655f177fa76ef7ef6926486911
ManUtopiK <emmanuel.salomon@free.fr>
2012-09-26 18:13:16 +0200

    Better river

--- views/default/river/object/markdown_wiki/create.php
+++ views/default/river/object/markdown_wiki/create.php
@@ -1,17 +1,17 @@
 <?php
 /**
- *	Elgg-markdown_wiki plugin
+ *	Elgg-markdown_wiki plugin
  *	@package elgg-markdown_wiki
  *	@author Emmanuel Salomon @ManUtopiK
  *	@license GNU Affero General Public License, version 3 or late
  *	@link https://github.com/ManUtopiK/elgg-markdown_wiki
  *
- *	Elgg-markdown_wiki river view
+ *	Elgg-markdown_wiki river create
  **/
 
 $object = $vars['item']->getObjectEntity();
 $excerpt = strip_tags($object->description);
-$excerpt = elgg_get_excerpt($excerpt);
+$excerpt = elgg_get_excerpt($excerpt, 140);
 
 echo elgg_view('river/elements/layout', array(
 	'item' => $vars['item'],
--- views/json/river/object/markdown_wiki/create.php
+++ views/json/river/object/markdown_wiki/create.php
@@ -8,12 +8,13 @@
  *
  *	Elgg-markdown_wiki river view
  **/
+global $jsonexport;
 
 $object = $vars['item']->getObjectEntity();
 $excerpt = strip_tags($object->description);
-$excerpt = elgg_get_excerpt($excerpt);
+$excerpt = elgg_get_excerpt($excerpt, 140);
 
-echo elgg_view('river/elements/layout', array(
-	'item' => $vars['item'],
-	'message' => $excerpt,
-));
+$vars['item']->summary = elgg_view('river/elements/summary', array('item' => $vars['item']), FALSE, FALSE, 'default');
+$vars['item']->message = $excerpt;
+
+$jsonexport['activity'][] = $vars['item'];
